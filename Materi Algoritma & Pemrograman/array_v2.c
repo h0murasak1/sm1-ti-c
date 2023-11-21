ada2bb26c16b3d6b83206eba5e21b974495cf287
homurasaki <farhanfatahillah10oib@gmail.com>
2023-11-21 11:16:03 +0700

    message

--- Materi Algoritma & Pemrograman/array_v2.c
+++ Materi Algoritma & Pemrograman/array_v2.c
@@ -3,16 +3,29 @@
 
 int main()
 {
-    int i, a[10];
-    for (i = 1; i <= 5; i++)
+    int i, n, a[10];
+    int sum = 0; // Inisialisasi sum dengan 0
+    float rtrt;
+
+    printf("Masukkan Banyak Angka : ");
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++) // Ubah indeks mulai dari 0
     {
-        printf("A[%d] : ", i);
+        printf("Masukkan Angka ke-[%d] : ", i + 1);
         scanf("%d", &a[i]);
+        sum += a[i]; // Tambahkan nilai ke sum di sini
     }
+
     printf("\n\n\n");
-    for (i=1; i<=5; i++)
+    for (i = 0; i < n; i++) // Ubah indeks mulai dari 0
     {
-        printf("A[%d] : %d\n", i, a[i]);
+        printf("Angka ke-[%d] Adalah : %d\n", i + 1, a[i]);
     }
+
+    rtrt = (float)sum / n; // Ubah sum menjadi float untuk menghindari pembagian integer
+
+    printf("Rata Rata Nilai Adalah : %.2f\n", rtrt);
+
     return 0;
 }
