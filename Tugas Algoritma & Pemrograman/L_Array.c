45ac9e135bdc7c0d63d284c11fb2229a014d7d6f
homurasaki <farhanfatahillah10oib@gmail.com>
2023-11-22 08:48:19 +0700

    message

---
+++ Tugas Algoritma & Pemrograman/L_Array.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i, n, a[10];
+    int angka = 0; // Inisialisasi angka dengan 0
+    float rtrt;
+
+    printf("Masukkan Banyak Angka : ");
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++)
+    {
+        printf("Masukkan Angka ke-[%d] : ", i + 1);
+        scanf("%d", &a[i]);
+        // angka += a[i];
+    }
+
+    printf("\n\n\n");
+    for (i = 0; i < n; i++)
+    {
+        if (a[i] % 2 == 0)
+        {
+            printf("Genap Array Ke-[%d] Adalah : %d\n", i + 1, a[i]);
+        }
+    }
+
+    /*rtrt = (float)angka / n; // Ubah angka menjadi float untuk menghindari penjumlahan integer
+
+    printf("Rata Rata Nilai Adalah : %.2f", rtrt);*/
+
+    return 0;
+}
